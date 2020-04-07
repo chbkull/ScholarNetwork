25e36ef671ac3fd14f45f4591ac5780120c10641
Christopher Kull <chbkull@gmail.com>
2020-04-07 18:50:36 -0500

    Created API application, article endpoint.

--- scholarnetwork/scholarnetwork/settings.py
+++ scholarnetwork/scholarnetwork/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'scholarnetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scholarnetwork',
+        'USER': 'root',
+        'PASSWORD': 'scholarnetwork',
+        'host': 'localhost',
+        'PORT': '3306',
     }
 }
 
