51b1b2a36885fa4271f1a1ecfbbca83e79ba6741
Christopher Kull <chbkull@gmail.com>
2020-04-09 20:40:47 -0500

    api/articles/id endpoint

--- scholarnetwork/api/urls.py
+++ scholarnetwork/api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('api/articles/', views.ArticleListCreate.as_view()),
-    path('api/articles/', views.request_article),
+    # path('api/articles/', views.request_article),
+    path('api/articles/<int:pk>', views.article_detail),
     path('api/authors/', views.AuthorListCreate.as_view()),
 ]
