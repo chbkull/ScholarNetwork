4325cd5e244a76880932bce6557204cd7d41255e
Christopher Kull <chbkull@gmail.com>
2020-04-09 22:47:32 -0500

    Added search API calls.

--- scholarnetwork/api/serializers.py
+++ scholarnetwork/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import Article, Author
+from .models import Article, Author, User
+
+# pylint: disable=no-member
 
 class ArticleSerializer(serializers.ModelSerializer):
     
@@ -21,6 +23,36 @@ class ArticleSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'affiliation', 'citedby', 'pub_title', 'pub_year', 'citations', 'pub_author', 'eprint')
 
 class AuthorSerializer(serializers.ModelSerializer):
+    
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.affiliation = validated_data.get('affiliation', instance.affiliation)
+        instance.citedby = validated_data.get('citedby', instance.citedby)
+        instance.attributes = validated_data.get('attributes', instance.attributes)
+        instance.page = validated_data.get('page', instance.page)
+        instance.email = validated_data.get('email', instance.email)
+        instance.interests = validated_data.get('interests', instance.interests)
+        instance.url_picture = validated_data.get('url_picture', instance.url_picture)
+    
     class Meta:
         model = Author
         fields = ('id', 'name', 'affiliation', 'citedby', 'attributes', 'page', 'email', 'interests', 'url_picture')
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        instance.affiliation = validated_data.get('affiliation', instance.affiliation)
+        instance.history = validated_data.get('history', instance.history)
+        instance.interests = validated_data.get('interests', instance.interests)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'affiliation', 'history', 'interests')
--- scholarnetwork/api/views.py
+++ scholarnetwork/api/views.py
@@ -1,25 +1,19 @@
 from django.shortcuts import render
-from .models import Article, Author
-from .serializers import ArticleSerializer, AuthorSerializer
+from .models import Article, Author, User
+from .serializers import ArticleSerializer, AuthorSerializer, UserSerializer
 from rest_framework import generics
 
 # pylint: disable=no-member
 
-# class ArticleListCreate(generics.ListCreateAPIView):
-#     queryset = Article.objects.get_article(10)
-#     # queryset = Article.objects.all()[:10]
-#     serializer_class = ArticleSerializer
-
-class AuthorListCreate(generics.ListCreateAPIView):
-    queryset = Author.objects.all()[:10]
-    serializer_class = AuthorSerializer
+# class AuthorListCreate(generics.ListCreateAPIView):
+#     queryset = Author.objects.all()[:10]
+#     serializer_class = AuthorSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
 
 from django.core import serializers
-from rest_framework.renderers import JSONRenderer
 
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -29,8 +23,8 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 
 class ArticleList(APIView):
-    def get(self, request, foramt=None):
-        articles = Article.objects.raw("SELECT * FROM articles LIMIT 10")
+    def get(self, request, format=None):
+        articles = Article.objects.raw("SELECT * FROM articles LIMIT 100")
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
     
@@ -65,4 +59,110 @@ class ArticleDetail(APIView):
     def delete(self, request, pk, format=None):
         article = self.get_object(pk)
         article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ArticleSearchTitle(APIView):
+    def get(self, request, searchby, format=None):
+        # Translator: xgettext:no-python-format
+        # articles = Article.objects.raw("SELECT * FROM articles WHERE pub_title LIKE %%s%", searchby)
+        articles = Article.objects.filter(pub_title__icontains=searchby)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+class ArticleSearchAuthor(APIView):
+    def get(self, request, searchby, format=None):
+        # Translator: xgettext:no-python-format
+        # articles = Article.objects.raw("SELECT * FROM articles WHERE pub_title LIKE %%s%", searchby)
+        articles = Article.objects.filter(name__icontains=searchby)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+class AuthorList(APIView):
+    def get(self, request, format=None):
+        authors = Author.objects.raw("SELECT * FROM authors LIMIT 100")
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AuthorDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Author.objects.raw("SELECT * FROM authors WHERE id = %s", [pk])
+        except Author.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def get(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = AuthorSerializer(author, many=True)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        author = self.get_object(pk)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AuthorSearchName(APIView):
+    def get(self, request, searchby, format=None):
+        authors = Author.objects.filter(name__icontains=searchby)
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+class AuthorSearchAffiliation(APIView):
+    def get(self, request, searchby, format=None):
+        authors = Author.objects.filter(affiliation__icontains=searchby)
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        users = User.objects.raw("SELECT * FROM users LIMIT 100")
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.raw("SELECT * FROM users WHERE id = %s", [pk])
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
