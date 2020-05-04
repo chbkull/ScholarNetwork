032bea27b33bbbf16dc83fdbe6fb7848e8c4febf
Christopher Kull <chbkull@gmail.com>
2020-05-04 16:13:06 -0500

    Added missing cascades.

--- mysqldb/creationscripts/visited_by_create.sql
+++ mysqldb/creationscripts/visited_by_create.sql
@@ -2,7 +2,7 @@ CREATE TABLE visited_by (
 	user_id INT NOT NULL,
     article_id INT NOT NULL,
     timestamp TIME,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (article_id) REFERENCES articles(id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (user_id, article_id)
 )
